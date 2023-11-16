1d4dba5d100ccf2f18b65c05edcd3f3983486504
km_nuri <kmw6078@techdna.kr>
2023-11-16 19:33:12 +0900

    강민_프로젝트 생성에 따른 최초 커밋 1. build gradle 2. springboot + thymeleaf 3. 레이아웃 작업중

---
+++ RE-Platform2023/src/main/java/kr/techdna/replatform2023/RePlatform2023Application.java
@@ -0,0 +1,13 @@
+package kr.techdna.replatform2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RePlatform2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RePlatform2023Application.class, args);
+    }
+
+}
---
+++ RE-Platform2023/src/test/java/kr/techdna/replatform2023/RePlatform2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.techdna.replatform2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RePlatform2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
